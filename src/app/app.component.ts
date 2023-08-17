ec13d057f89ddff649562d0ccfb6e6de206cdd34
cherryApp <j.cserko@gmail.com>
2023-08-17 18:12:00 +0200

    start lesson-05

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,68 +10,6 @@ import { BehaviorSubject } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-  ticketService: TicketService = inject(TicketService);
-
-  title = 'angular-directives';
-
-  isSearchBarVisible: boolean = false;
-
-  filterPhrase: string = '';
-
-  tickets: Ticket[] = [
-    {
-      id: 1,
-      checked: false,
-      flightNumber: 'WA12345',
-      seat: 'D3',
-      service: 'tourist',
-    },
-    {
-      id: 2,
-      checked: true,
-      flightNumber: 'WA12345',
-      seat: 'D4',
-      service: 'tourist',
-    },
-    {
-      id: 3,
-      checked: false,
-      flightNumber: 'WA12345',
-      seat: 'F5',
-      service: 'tourist',
-    },
-  ];
-
-  btnGroup: IBtn[] = [
-    { name: 'show', type: 'info', icon: 'fa-eye' },
-    { name: 'remove', type: 'danger', icon: 'fa-trash' },
-  ];
-
-  tickets$ = this.ticketService.list$;
-
-  selectedTicket$ = this.ticketService.one$;
-
-  ngOnInit(): void {
-    this.ticketService.dispatch('getAll');
-
-    this.selectedTicket$.subscribe( console.log );
-  }
-
-  toggleSearchBar(): void {
-    this.isSearchBarVisible = !this.isSearchBarVisible;
-  }
-
-  onGroupClick(details: IBtnGroupOutput) {
-    switch(details.name) {
-      case 'remove':
-        this.ticketService.dispatch('delete', (details.data as Ticket));
-        break;
-      case 'show':
-        this.ticketService.dispatch('get', details.data.id);
-        break;
-    }
-  }
+export class AppComponent {
 
 }
--- src/app/page/tickets/tickets.component.ts
+++ src/app/page/tickets/tickets.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IBtn, IBtnGroupOutput } from 'src/app/common/btn-group/btn-group.component';
+import { Ticket } from 'src/app/model/ticket';
+import { TicketService } from 'src/app/service/ticket.service';
 
 @Component({
   selector: 'app-tickets',
@@ -6,5 +9,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./tickets.component.scss']
 })
 export class TicketsComponent {
+  ticketService: TicketService = inject(TicketService);
 
+  title = 'angular-directives';
+
+  isSearchBarVisible: boolean = false;
+
+  filterPhrase: string = '';
+
+  tickets: Ticket[] = [
+    {
+      id: 1,
+      checked: false,
+      flightNumber: 'WA12345',
+      seat: 'D3',
+      service: 'tourist',
+    },
+    {
+      id: 2,
+      checked: true,
+      flightNumber: 'WA12345',
+      seat: 'D4',
+      service: 'tourist',
+    },
+    {
+      id: 3,
+      checked: false,
+      flightNumber: 'WA12345',
+      seat: 'F5',
+      service: 'tourist',
+    },
+  ];
+
+  btnGroup: IBtn[] = [
+    { name: 'show', type: 'info', icon: 'fa-eye' },
+    { name: 'remove', type: 'danger', icon: 'fa-trash' },
+  ];
+
+  tickets$ = this.ticketService.list$;
+
+  selectedTicket$ = this.ticketService.one$;
+
+  ngOnInit(): void {
+    this.ticketService.dispatch('getAll');
+
+    this.selectedTicket$.subscribe(console.log);
+  }
+
+  toggleSearchBar(): void {
+    this.isSearchBarVisible = !this.isSearchBarVisible;
+  }
+
+  onGroupClick(details: IBtnGroupOutput) {
+    switch (details.name) {
+      case 'remove':
+        this.ticketService.dispatch('delete', (details.data as Ticket));
+        break;
+      case 'show':
+        this.ticketService.dispatch('get', details.data.id);
+        break;
+    }
+  }
 }
